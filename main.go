386719d8f1a9f15f993fc23f970a1e49f6da088b
Seiya Miyata <odradek38@gmail.com>
2024-01-16 16:49:17 +0900

    Fix import sentences

--- main.go
+++ main.go
@@ -3,26 +3,27 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 func main() {
 	type value struct {
-		mu sync.Mutex
+		mu    sync.Mutex
 		value int
 	}
-	
-	var wg sync.WaiteGroup
-	
-	printSum := func(v1, v2, *value) {
+
+	var wg sync.WaitGroup
+
+	printSum := func(v1, v2 *value) {
 		defer wg.Done()
 		v1.mu.Lock()
 		defer v1.mu.Unlock()
 
-		time.Sleep(2*time.Second)
+		time.Sleep(2 * time.Second)
 		v2.mu.Lock()
 		defer v2.mu.Unlock()
 
-		fmt.Printf("sum=%v\n", v1.value + v2.value)
+		fmt.Printf("sum=%v\n", v1.value+v2.value)
 	}
 
 	var a, b value
